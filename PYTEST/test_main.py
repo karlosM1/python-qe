284fdb79ea07eda3dca2678b9cb6151512c4d3e5
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2026-03-06 16:01:51 +0800

    class pytest

--- PYTEST/test_main.py
+++ PYTEST/test_main.py
@@ -1,11 +1,16 @@
-from main import add, divide
+from main import UserManager
 import pytest
 
-def test_add():
-  assert add(2,3) == 5, "2 + 3 should be 5"
-  assert add(-1,1) == 0, "-1 + 1 should be 0"
-  assert add(0,0) == 0, "0  + 0 should be 0"
+@pytest.fixture
+def user_manager():
+    """Creates a fresh instance of UserManager before each test."""
+    return UserManager()
+  
+def test_add_user(user_manager):
+    assert user_manager.add_user("john_doe", "john@example.com") == True
+    assert user_manager.get_user("john_doe") == "john@example.com"
 
-def test_divide():
-  with pytest.raises(ValueError, match="Cannot divide by zero"):
-    divide(10,0)
+def test_add_duplicate_user(user_manager):
+    user_manager.add_user("john_doe", "john@example.com")
+    with pytest.raises(ValueError):
+      user_manager.add_user("john_doe", "john@example.com")
